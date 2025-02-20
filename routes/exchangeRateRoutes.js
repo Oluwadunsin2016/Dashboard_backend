eb344c95cd9e2f86a4ee67ea519a75398da5d3f7
Oluwadunsin <oluwadunsin2016@gmail.com>
2025-02-20 18:21:06 +0100

    Updated

--- routes/exchangeRateRoutes.js
+++ routes/exchangeRateRoutes.js
@@ -3,6 +3,13 @@ const exchange_rates = require('../models/exchangeRate');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");  // Allow all origins
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+  
 // Create or update exchange rate
 router.post('/set-rate', async (req, res) => {
   const { fromCountry, toCountry, rate } = req.body;
