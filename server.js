2680549cf06896e5a229f1488167025fbee8b23d
Oluwadunsin <oluwadunsin2016@gmail.com>
2025-02-20 17:50:49 +0100

    updated

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-
-app.use(cors());
+app.use(cors({origin:'*',credentials: true}));
+  
+  app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/api', exchangeRateRoutes);
 
